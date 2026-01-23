0b3808b82d0051745d3902a99f3af4958959bcfd
Isaias Matos <isamatoshdez@gmail.com>
2026-01-22 21:04:37 -0400

    Tarea1.1

--- Microcontroladores/Example2/main/main.c
+++ Microcontroladores/Example2/main/main.c
@@ -6,7 +6,8 @@
 void app_main(void)
 {
     while (1) {
-        printf("Que dificil es Microcontroladoreee!\n");
+        printf("Que dificil es MicrocontroladoreS!\n");
+        printf("I'M BATMAN\n\n");
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
